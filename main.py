4f8773f6a4dd8a813968080f52ad282cee1b3f06
liinalib <liinaliber@gmail.com>
2023-04-02 00:29:22 +0300

    importing second func commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from funcs import add_numbers
+from funcs import add_numbers, sub_numbers
 
 added_numbers = add_numbers(6, 9)
-print(result)
+print(added_numbers)
+
+subs_numbers = sub_numbers(7, 5)
+print(subs_numbers)
